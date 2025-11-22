72129333f458e5d2c50aa7d15f7dd9d9560a264c
irie-yamashita <irie.yamashita.7e8@itb.cat>
2025-11-22 18:05:23 +0100

    add logica buidar carret + modals

--- src/components/Carret.vue
+++ src/components/Carret.vue
@@ -7,17 +7,31 @@ const costTotal = computed(() =>
   carret.value.reduce((total, producte) => total + producte.preu, 0)
 );
 
+const handleBuidarCarret = () => {
+    const confirmation = confirm('Estàs segur@ que vols buidar el carret?');
+
+    if(confirmation) {
+        carret.value.length = 0;
+    }
+}
+
 
 </script>
 
 <template>
    <div class="flex flex-col justify-between gap-4 border-2 border-teal-950 rounded-md p-4 h-full">
     <header class="space-y-2">
-        <h2 class="text-teal-900 text-2xl font-bold">Carret</h2>
+        <div class="flex justify-between items-end">
+            <h2 class="text-teal-900 text-2xl font-bold">Carret</h2>
+            <button @click="handleBuidarCarret" class="cursor-pointer hover:brightness-110 hover:scale-105 transition-transform">
+                <img src="../assets/images/cart-cross.svg" class="text-teal-500 size-7" alt="buidar carret">
+            </button>
+        </div>
         <ul class="flex flex-col gap-2">
              <carret-item v-for="producte in carret" :key="producte.id" :producte="producte"></carret-item>
         </ul>
     </header>
     <p class="border-t w-full text-end pt-2">Total: {{ costTotal }}€</p>
+
    </div>
 </template>
--- src/components/ProducteItem.vue
+++ src/components/ProducteItem.vue
@@ -7,8 +7,12 @@ const props = defineProps({
 
 const emit = defineEmits(['add-product'])
 
-const onAfegirProducte = () => {
-    emit('add-product', props.producte)
+const onAfegirProducte = (producte) => {
+    const confirmation = confirm("Vols afegir '"+producte.nom+"' al teu carret?");
+
+    if(confirmation) {
+        emit('add-product', props.producte)
+    }
 }
 
 </script>
@@ -20,7 +24,7 @@ const onAfegirProducte = () => {
             <h3>{{ props.producte.nom }}</h3>
             <p class="text-teal-900 font-bold text-[18px]">{{ props.producte.preu }}€</p>
         </div>
-        <button @click="onAfegirProducte" class="btn w-fit bg-teal-900!">Afegir al carret</button>
+        <button @click="onAfegirProducte(producte)" class="btn w-fit bg-teal-900!">Afegir al carret</button>
     </li>
 
 </template>
